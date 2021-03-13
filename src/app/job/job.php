fa9f1ee1def5744f6d6af7ed87247ea3685ed062
Angad Thandi <angadthandi8@gmail.com>
2021-03-13 15:58:10 -0700

    Implement Job Class

--- src/app/job/job.php
+++ src/app/job/job.php
@@ -2,8 +2,58 @@
 
 class Job {
 
-    public function __construct() {
-        
+    public const ROOT = 0;
+    public const ADMIN = 1;
+    public const USER = 2;
+
+    private Int $id;
+    private String $name;
+    private Int $duration;
+    private Int $priority;
+    private Int $deadline;
+    private Int $userType; // ROOT, ADMIN, USER
+
+    private static Int $idIncrementor = 0;
+
+    public function __construct(
+        String $name,
+        Int $duration,
+        Int $priority,
+        Int $deadline,
+        Int $userType,
+    ) {
+        static::$idIncrementor++;
+
+        $this->id = static::$idIncrementor;
+        $this->name = $name;
+        $this->duration = $duration;
+        $this->priority = $priority;
+        $this->deadline = $deadline;
+        $this->userType = $userType;
+    }
+
+    public function GetID(): Int {
+        return $this->id;
+    }
+
+    public function GetName(): String {
+        return $this->name;
+    }
+
+    public function GetDuration(): Int {
+        return $this->duration;
+    }
+
+    public function GetPriority(): Int {
+        return $this->priority;
+    }
+
+    public function GetDeadline(): Int {
+        return $this->deadline;
+    }
+
+    public function GetUserType(): Int {
+        return $this->userType;
     }
 
 }
